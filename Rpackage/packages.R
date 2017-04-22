6238bc4489af2f682b85a28a63330c326570eaf3
Manuel Parra <manuparra@gmail.com>
2017-04-22 19:32:04 +0200

    packages

---
+++ Rpackage/packages.R
@@ -0,0 +1,14 @@
+# Resources
+library(RMySQL)
+library(knitr)
+library(psych)
+library(plot3D)
+library(hexbin)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+library(RColorBrewer)
+library("calibrate")
+library(corrplot)
+library(aod)
+
